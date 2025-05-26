f0fcb56f20f10e57670547e09852e5e16f90658b
juanesGY500 <130929164+juanesGY500@users.noreply.github.com>
2025-05-26 20:04:59 +0000

    ola

--- pokechit/vite-project/src/componentes/login/index.jsx
+++ pokechit/vite-project/src/componentes/login/index.jsx
@@ -1,9 +1,48 @@
-function login(){
+import { useState } from 'react';
+import { supabase } from '../../supabase';
+import { useNavigate } from 'react-router-dom';
 
-    return(
-        <>
-            <h1>Login</h1>
-        </>
-    )
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+
+    if (error) {
+      alert("Usuario o contraseña no válido");
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Iniciar sesión</button>
+      </form>
+      <h2>No tiene cuenta</h2>
+      <button onClick={() => navigate("/register")}>Registrarse</button>
+    </div>
+  );
 }
-export default login
+
+export default Login;
